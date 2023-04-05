29a54ffe7be23f5f4e4a447935e7175b62ac1357
MBilik <dexeraz@gmail.com>
2023-04-05 12:00:33 +0200

    Revert "Incorrect hook call fix :)"
    This reverts commit 28bce7804f925f9208acb487f503911ca6edf970.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,31 @@
+<<<<<<< HEAD
 // src/components/Search.tsx
 import React, { useState } from "react";
 import { useSearchPeopleQuery } from "../starWarsApi";
+=======
+import React from "react";
+import { useAppSelector, useAppDispatch } from "../store/hooks";
+import { setPeople } from "../store/slices/searchSlice";
+import { fetchPerson, IPeople } from "../api";
+>>>>>>> parent of 28bce78 (Incorrect hook call fix :))
 import SearchInput from "./SearchInput";
 import SearchResults from "./SearchResults";
 
 const Search = () => {
+<<<<<<< HEAD
   const [searchTerm, setSearchTerm] = useState("");
   const { data: people, error, isLoading } = useSearchPeopleQuery(searchTerm);
   const handleSearch = () => {};
+=======
+  const people = useAppSelector((state) => state.search.people);
+  const dispatch = useAppDispatch();
+
+  const handleSearch = async () => {
+    const searchTerm = useAppSelector((state) => state.search.searchTerm);
+    const fetchedPeople = await fetchPerson(searchTerm);
+    dispatch(setPeople(fetchedPeople));
+  };
+>>>>>>> parent of 28bce78 (Incorrect hook call fix :))
 
   return (
     <div>
