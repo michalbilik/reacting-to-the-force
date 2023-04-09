bf537aebd7f62a2d7270d4a63415f3a61753e8b4
MBilik <dexeraz@gmail.com>
2023-04-09 19:17:27 +0200

    Moved the logic of fetching out out PersonDetails

--- src/components/PersonDetails.tsx
+++ src/components/PersonDetails.tsx
@@ -1,13 +1,8 @@
 import { useLocation, useNavigate } from "react-router-dom";
-import {
-  useGetPlanetQuery,
-  useGetFilmQuery,
-  useGetVehicleQuery,
-  useGetStarshipQuery,
-} from "../api/starWarsApi";
-import Loading from "./Loading";
 import { useState, useEffect } from "react";
 import { getIdFromUrl } from "../helpers/getIdFromUrl";
+import { usePersonData } from "../hooks/usePersonData";
+import Loading from "./Loading";
 
 const PersonDetails = () => {
   const [timeoutExceeded, setTimeoutExceeded] = useState(false);
@@ -15,6 +10,7 @@ const PersonDetails = () => {
   const navigate = useNavigate();
 
   const person = state.person;
+  const { homeworldData, films, vehicles, starships } = usePersonData(person);
   const personId = getIdFromUrl(person.url, "people");
   const imageUrl = `https://starwarsapibucket.s3.eu-central-1.amazonaws.com/${personId}.jpg`;
 
@@ -31,38 +27,6 @@ const PersonDetails = () => {
   const handleBackToSearch = () => {
     navigate("/");
   };
-
-  // Homeworld
-  const homeworldId = person.homeworld.split("/").slice(-2, -1)[0];
-  const { data: homeworldData } = useGetPlanetQuery(homeworldId);
-  // Films
-  const filmIds = person.films.map(
-    (film: string) => film.split("/").slice(-2, -1)[0]
-  );
-  const filmQueries = filmIds.map((id: string) => useGetFilmQuery(id));
-  const films = filmQueries
-    .map((query: ReturnType<typeof useGetFilmQuery>) => query.data?.title)
-    .filter(Boolean);
-
-  // Vehicles
-  const vehicleIds = person.vehicles.map(
-    (vehicle: string) => vehicle.split("/").slice(-2, -1)[0]
-  );
-  const vehicleQueries = vehicleIds.map((id: string) => useGetVehicleQuery(id));
-  const vehicles = vehicleQueries
-    .map((query: ReturnType<typeof useGetVehicleQuery>) => query.data?.name)
-    .filter(Boolean);
-
-  // Starships
-  const starshipIds = person.starships.map(
-    (starship: string) => starship.split("/").slice(-2, -1)[0]
-  );
-  const starshipQueries = starshipIds.map((id: string) =>
-    useGetStarshipQuery(id)
-  );
-  const starships = starshipQueries
-    .map((query: ReturnType<typeof useGetStarshipQuery>) => query.data?.name)
-    .filter(Boolean);
 
   if (!person) {
     return <div>Person not found</div>;
---
+++ src/hooks/usePersonData.ts
@@ -0,0 +1,48 @@
+import {
+  useGetPlanetQuery,
+  useGetFilmQuery,
+  useGetVehicleQuery,
+  useGetStarshipQuery,
+} from "../api/starWarsApi";
+
+export const usePersonData = (person: any) => {
+  // Homeworld
+  const homeworldId = person.homeworld.split("/").slice(-2, -1)[0];
+  const { data: homeworldData } = useGetPlanetQuery(homeworldId);
+
+  // Films
+  const filmIds = person.films.map(
+    (film: string) => film.split("/").slice(-2, -1)[0]
+  );
+  const filmQueries = filmIds.map((id: string) => useGetFilmQuery(id));
+  const films = filmQueries
+    .map((query: ReturnType<typeof useGetFilmQuery>) => query.data?.title)
+    .filter(Boolean);
+
+  // Vehicles
+  const vehicleIds = person.vehicles.map(
+    (vehicle: string) => vehicle.split("/").slice(-2, -1)[0]
+  );
+  const vehicleQueries = vehicleIds.map((id: string) => useGetVehicleQuery(id));
+  const vehicles = vehicleQueries
+    .map((query: ReturnType<typeof useGetVehicleQuery>) => query.data?.name)
+    .filter(Boolean);
+
+  // Starships
+  const starshipIds = person.starships.map(
+    (starship: string) => starship.split("/").slice(-2, -1)[0]
+  );
+  const starshipQueries = starshipIds.map((id: string) =>
+    useGetStarshipQuery(id)
+  );
+  const starships = starshipQueries
+    .map((query: ReturnType<typeof useGetStarshipQuery>) => query.data?.name)
+    .filter(Boolean);
+
+  return {
+    homeworldData,
+    films,
+    vehicles,
+    starships,
+  };
+};
